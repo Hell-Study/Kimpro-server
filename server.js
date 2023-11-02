d8ad54abead5422c2aeae73ae574eb49999e7504
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2023-11-03 00:26:27 +0900

    기본 포트 변경

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const morgan = require('morgan')
 require('dotenv').config()
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 const clients = []
 
 const corsOptions = {
