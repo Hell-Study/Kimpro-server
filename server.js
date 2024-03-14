3be34a624a31ab55d93427781a46e571ec03e07e
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-03-15 03:24:58 +0900

    Refactor: server.js 관심사 분리

--- server.js
+++ server.js
@@ -1,84 +1,25 @@
-const express = require('express')
-const { investing } = require('investing-com-api')
-const cors = require('cors')
-const morgan = require('morgan')
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const exchangeRateRouter = require("./routes/exchangeRate");
 
-const app = express()
-const PORT = process.env.PORT || 8080
-const clients = []
+dotenv.config();
 
-const whitelist = ['https://kimpro.site', 'http://localhost:3000', 'http://localhost:3001']
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not Allowed Origin!'))
-    }
-  },
-}
+const app = express();
+const PORT = process.env.PORT || 8080;
 
-app.use(cors(corsOptions))
-app.use(morgan('combined'))
+app.use(cors(require("./config/corsOptions")));
+app.use(morgan("combined"));
 
-let lastData = null
-let isFetching = false
+app.use("/exchange-rate", exchangeRateRouter);
 
-const fetchData = async () => {
-  if (isFetching) return
-
-  isFetching = true
-
-  try {
-    const allData = await investing('currencies/usd-krw', 'P1D', 'PT1M', 60, {
-      headless: 'true',
-      args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-gpu'],
-      ignoreHTTPSErrors: true,
-    })
-
-    const latestData = allData[allData.length - 1]
-
-    if (!lastData || latestData.price_close !== lastData.price_close) {
-      for (const client of clients) {
-        client.write(`data: ${JSON.stringify(latestData)}\n\n`)
-      }
-      lastData = latestData
-    }
-  } catch (err) {
-    console.error(err)
-    for (const client of clients) {
-      client.write(`error: ${err.message}\n\n`)
-    }
-  }
-  isFetching = false
-}
-
-app.get('/exchange-rate', (req, res) => {
-  res.setHeader('Content-Type', 'text/event-stream')
-  res.setHeader('Cache-Control', 'no-cache')
-  res.setHeader('Connection', 'keep-alive')
-  res.flushHeaders()
-
-  clients.push(res)
-
-  if (lastData) {
-    res.write(`data: ${JSON.stringify(lastData)}\n\n`)
-    console.log(`Client connected, total clients: ${clients.length}`)
-  }
-
-  req.on('close', () => {
-    const index = clients.indexOf(res)
-    if (index !== -1) {
-      clients.splice(index, 1)
-    }
-    console.log(`Client disconnected, total clients: ${clients.length}`)
-  })
-})
-
-setInterval(fetchData, 40000)
+app.get("/", (req, res) => {
+  res.send(
+    "Welcome to the API server. Use /exchange-rate to get exchange rate information."
+  );
+});
 
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`)
-  fetchData()
-})
+  console.log(`Server started on port ${PORT}`);
+});
